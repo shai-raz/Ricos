549b6320645cb33ffb95510f89afcdde83b0cc40
Shai Raz <shayy9999@gmail.com>
2022-03-02 21:47:26 +0200

    init

---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,56 @@
+import logo from '../img/logo.png'
+import SearchBar from './SearchBar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowAltCircleRight } from '@fortawesome/free-regular-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { faUserCircle } from '@fortawesome/free-regular-svg-icons'
+import { faCompass } from '@fortawesome/free-regular-svg-icons'
+import ricos from "../img/ricos.png"
+
+import '../css/header.css'
+import { faBars, faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <div className="header-inner-container">
+                <a href="/">
+                    <div className="header-logo">
+                        <img id="header-logo-img" src={logo} alt="Logo!" />
+                    </div>
+                </a>
+                <div>
+                    <SearchBar />
+                </div>
+                <div className="header-icons-container">
+
+                    <a href="/post/new">
+                        <FontAwesomeIcon icon={faPlusCircle} size="lg" />
+                    </a>
+
+                    <a href="/">
+                        <FontAwesomeIcon icon={faCompass} size="lg" />
+                    </a>
+
+                    <a href="/">
+                        <FontAwesomeIcon icon={faEnvelope} size="lg" />
+                    </a>
+
+                    <a href="/profile">
+                        <FontAwesomeIcon icon={faUserCircle} size="lg" />
+                    </a>
+
+                    <a href="/logout">
+                        <FontAwesomeIcon icon={faArrowAltCircleRight} size="lg" />
+                    </a>
+
+                </div>
+
+                <div className="header-burger-icon">
+                    <FontAwesomeIcon icon={faBars} size="2x" />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Header
