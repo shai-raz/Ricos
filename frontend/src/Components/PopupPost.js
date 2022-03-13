1a0718cfeb4eaf2c8ab83fbeba0c0632e7b1c333
Shai Raz <shayy9999@gmail.com>
2022-03-13 16:44:51 +0200

    pop up post now showing all recipe info

--- frontend/src/Components/PopupPost.js
+++ frontend/src/Components/PopupPost.js
@@ -2,31 +2,43 @@ import { useEffect } from 'react'
 import '../css/popuppost.css'
 import Comment from './Comment'
 import ProfilePicture from './ProfilePicture'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faEgg, faHamburger, faTimes, faUtensils, faUtensilSpoon, faXRay } from '@fortawesome/free-solid-svg-icons'
+import { faAddressBook } from '@fortawesome/free-regular-svg-icons'
 
 const PopupPost = (props) => {
-    const postId = props.postId
-    const authorId = props.authorId
-    const title = props.title
+    const recipe = props.recipe
+    const postId = recipe.id
+    const authorId = recipe.uid
+    const description = recipe.description
+    const ingredients = recipe.ingredients
+    const steps = recipe.steps
+    const img = recipe.img
+    const title = recipe.title
+    const numOfLikes = recipe.numOfLikes
     const authorName = props.authorName
-    const img = props.img
     //const numOfLikes = numOfLikesState ? numOfLikesState : props.numOfLikes
-    const description = props.description
     //const liked = likedState ? likedState : (props.liked === 1)
+
+    const hidePopup = () => {
+        props.setVisible(false)
+    }
 
     const keyPressed = (e) => {
         console.log(e.target, e.currentTarget)
-
     }
 
     const handleOutsideClick = (e) => {
         if (e.target === e.currentTarget)
-            props.setVisible(false)
+            hidePopup()
     }
+
+    
 
     useEffect(() => {
         const handleEsc = (e) => {
             if (e.keyCode === 27) // ESC
-            props.setVisible(false)
+                hidePopup()
         }
         window.addEventListener('keydown', handleEsc)
     }, [props.setVisible])
@@ -53,7 +65,52 @@ const PopupPost = (props) => {
                             </div>
                         </div>
 
-                       
+                        <div className="popuppost-left-container-header-right">
+                            <FontAwesomeIcon icon={faTimes} onClick={hidePopup} />
+                        </div>
+
+
+                    </div>
+                    <div className="popuppost-left-container-post">
+                        <div className="popuppost-left-container-post-description">
+                            {description}
+                        </div>
+                        <div className="popuppost-left-container-post-img">
+                            <img src={'data:image/png;base64, ' + img} alt="post" />
+                        </div>
+
+
+                        <div className="popuppost-left-container-post-ingredients">
+                            <h3>Ingredients</h3>
+                            <ul>
+                                {ingredients.map((ingredient, i) => {
+                                    return (
+                                        <li key={i}>
+                                            <span className="list-fontawesome-icon">
+                                                <FontAwesomeIcon icon={faEgg} />
+                                            </span>
+                                            {ingredient.amount} {ingredient.unit} {ingredient.ingredient}
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+
+                        <div className="popuppost-left-container-post-steps">
+                            <h3>Steps</h3>
+                            <ul>
+                                {steps.map((step, i) => {
+                                    return (
+                                        <li key={i}>
+                                            <span className="list-fontawesome-icon">
+                                                <FontAwesomeIcon icon={faUtensilSpoon} />
+                                            </span>
+                                            {step}
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <div className="popuppost-right-container">
