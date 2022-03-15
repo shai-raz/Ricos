2face591b503bb0a0eed038afc96ca3bc0e79fcd
Shai Raz <shayy9999@gmail.com>
2022-03-15 09:30:54 +0200

    Changed recipe blueprint - now contains liked, authorName and profilePic

--- frontend/src/helpers/Recipe.js
+++ frontend/src/helpers/Recipe.js
@@ -1,6 +1,8 @@
-function Recipe(id, authorId, date, title, description, img, ingredients, steps, numOfLikes) {
+function Recipe(id, authorId, authorName, profilePic, date, title, description, img, ingredients, steps, numOfLikes, liked = 0) {
     this.id = id
     this.authorId = authorId
+    this.authorName = authorName
+    this.profilePic = profilePic
     this.date = date
     this.title = title
     this.description = description
@@ -8,6 +10,7 @@ function Recipe(id, authorId, date, title, description, img, ingredients, steps,
     this.ingredients = JSON.parse(ingredients)
     this.steps = JSON.parse(steps)
     this.numOfLikes = numOfLikes
+    this.liked = liked
 }
 
 export default Recipe
