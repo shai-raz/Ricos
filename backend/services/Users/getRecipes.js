6056efd9fcc9a2235086fa81fd93db5eb1a6f2e5
Shai Raz <shayy9999@gmail.com>
2022-03-15 09:27:55 +0200

    Recipe now also contains authorName and profilePic

--- backend/services/Users/getRecipes.js
+++ backend/services/Users/getRecipes.js
@@ -1,11 +1,18 @@
-const db = require("../db");
+const db = require("../db")
 
+/* Get all recipes a user has posted */
 async function getRecipes(uid) {
-  const result = await db.query("SELECT * FROM `Recipe` WHERE uid = ?", [uid]);
+  const result = await db.query(
+    "SELECT R.*, CONCAT_WS(' ', U.firstName, U.lastName) as fullName, U.profilePic \
+      FROM `Recipe` R \
+      LEFT JOIN `Users` U ON U.uid = R.uid \
+      WHERE R.uid = ?",
+    [uid]
+  )
 
-  return { result };
+  return { result }
 }
 
 module.exports = {
   getRecipes,
-};
+}
